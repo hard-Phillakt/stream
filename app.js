626c4982d30287dbbe6ee67862c18e9b86c74482
hard-phillakt <hard-phillakt@mail.ru>
2020-08-31 16:46:08 +0300

    fewfweff34

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.get('/', (req, res) => {
 
   res.end(`
-  <!DOCTYPE html>
+    <!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
@@ -16,60 +16,71 @@ app.get('/', (req, res) => {
         <title>Document</title>
         <script src="https://cdn.bootcss.com/flv.js/1.5.0/flv.min.js"></script>
         <style>
-            video {
-                width: 600px;
-                margin: 30px;
-            }
+          video {
+            width: 800px;
+            margin: 30px;
+          }
     
-            .videoElement__wrapp {
-                display: flex;
-            }
+          .videoElement__wrapp {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+    
+          .videoElement__wrapp .control {
+            display: flex;
+            justify-content: center;
+          }
+    
+          button {
+            cursor: pointer;
+            margin: 0 15px;
+            padding: 5px 50px;
+          }
+    
+          #play {
+            background: green;
+            color: #fff;
+          }
+    
+          #pause {
+            margin: 0 15px;
+            background: red;
+            color: #fff;
+          }
         </style>
       </head>
       <body>
-        
         <div class="videoElement__wrapp">
-            <video id="videoElement"></video>
-    
-            <video id="videoElement2"></video>
+          <video id="videoElement"></video>
+          <div class="control">
+            <button id="play">Play</button>
+            <button id="pause">Pause</button>
+          </div>
         </div>
     
+        <script>
+          if (flvjs.isSupported()) {
+            var videoElement = document.getElementById("videoElement");
+            var flvPlayer = flvjs.createPlayer({
+              type: "flv",
+              url: "ws://95.213.224.3:5000/cctv/stream.flv",   
+            });
+            flvPlayer.attachMediaElement(videoElement);
+            flvPlayer.load();
+          }
     
-        
-      <script>
-        if (flvjs.isSupported()) {
-          var videoElement = document.getElementById("videoElement");
-          var flvPlayer = flvjs.createPlayer({
-            type: "flv",
-            url: "ws://95.213.224.3:5000/cctv/stream.flv",
-            //url: "ws://localhost:5000/cctv/stream.flv",
-          });
-          flvPlayer.attachMediaElement(videoElement);
-          flvPlayer.load();
-        }
-  
-        var videoElementBnt = document.querySelector('#videoElement');
-        videoElementBnt.onclick = function(){
-          flvPlayer.play();
-        }
-  
-  
-        if (flvjs.isSupported()) {
-          var videoElement2 = document.getElementById("videoElement2");
-          var flvPlayer2 = flvjs.createPlayer({
-            type: "flv",
-            url: "ws://95.213.224.3:5000/cctv/stream2.flv",
-            //url: "ws://localhost:5000/cctv/stream2.flv",
-          });
-          flvPlayer2.attachMediaElement(videoElement2);
-          flvPlayer2.load();
-        }
-  
-        var videoElement2Bnt = document.querySelector('#videoElement2');
-        videoElement2Bnt.onclick = function(){
-          flvPlayer2.play();
-        }
-  
+          var play = document.querySelector("#play");
+          var pause = document.querySelector("#pause");
+    
+          play.onclick = function () {
+            flvPlayer.play();
+          };
+    
+          pause.onclick = function () {
+            flvPlayer.play();
+          };
         </script>
       </body>
     </html>
@@ -101,18 +112,19 @@ const config = {
         app: 'cctv',
         mode: 'static',
         // edge: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
-        edge: 'rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream',
+        edge: 'rtsp://operator:Operator46!@188.170.54.115:554/cam/realmonitor?channel=1&subtype=0',
         name: 'stream',
         rtsp_transport: 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
       },
-      {
-        app: 'cctv',
-        mode: 'static',
-        // edge: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
-        edge: 'rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream',
-        name: 'stream2',
-        rtsp_transport: 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
-      },
+      // {
+      //   app: 'cctv',
+      //   mode: 'static',
+      //   // edge: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
+      //   edge: 'rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream',
+      //   name: 'stream2',
+      //   rtsp_transport: 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
+      // },
+      // url: "ws://localhost:5000/cctv/stream.flv",
       // {
       //   app: 'cctv',
       //   mode: 'static',
